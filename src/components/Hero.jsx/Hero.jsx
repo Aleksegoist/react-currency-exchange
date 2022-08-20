b8ae97e210a27a082674886f7ff8c92649fd1227
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-08-20 15:41:55 +0300

    add

---
+++ src/components/Hero.jsx/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <div className={style.hero}>
+      <div className={style.container}></div>
+    </div>
+  );
+};
+
+export default Hero;
